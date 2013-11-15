1c30241e1208020dfae03245346d6eee3e36e844
Killua Huang <killua_hzl@163.com>
2013-11-15 13:49:12 +0800

    Update python setup script version.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup, Extension
 NLPIR_module = Extension('_NLPIR',sources=['NLPIR_wrap.cxx'], libraries = ['NLPIR'])
 
 setup(name = 'NLPIR',
-    version = '1.0',
+    version = '2.0',
     author = 'Killua',
     description = 'Python for NLPIR',
     ext_modules = [NLPIR_module],
