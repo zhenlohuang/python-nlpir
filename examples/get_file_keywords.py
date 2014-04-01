6d8ac958676b1b4c53821370e296000c3debbcce
killuahzl <killua_hzl@163.com>
2014-04-02 00:04:13 +0800

    Update examples

--- examples/get_file_keywords.py
+++ examples/get_file_keywords.py
@@ -2,10 +2,12 @@
 #encoding: utf8
 
 from PyNLPIR import *
+import os
 
 if __name__ == '__main__':
 
-    if init('../', Constants.CodeType.GBK_CODE):
+    init_dir = os.path.join(os.path.dirname(__file__), os.path.pardir)
+    if init(init_dir, Constants.CodeType.UTF8_CODE):
         print 'NLPIR initialization succeed.'
     else:
         raise 'NLPIR initialization fail.'
